7723aeed3750c700416ee60ce55d953b3c11009e
Deepanshu Kumar <anshudeep077@outlook.com>
2023-08-12 11:20:35 +0530

    mobile responsiveness fix

--- index.js
+++ index.js
@@ -4,65 +4,61 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click", function()
     {
-        var buttonInnerHTML = this.innerHTML;
+        var buttonInnerHTML = this.innerHTML.toLowerCase();
         console.log(buttonInnerHTML);
         makesound(buttonInnerHTML);
     });
 }
 
-
 // Detecting KeyPress
 
-document.addEventListener("keypress", function(event)
+document.addEventListener("keydown", function(event)
 {
     makesound(event.key);
 });
-
 
 function makesound(key)
 {
  
     switch(key)
     {
-        case "Q":
+        case "q":
             var snare1 = new Audio('./sounds/tom-1.mp3');
             snare1.play();
             break;
 
-        case "W":
+        case "w" :
             var snare2 = new Audio('./sounds/snarew.mp3');
             snare2.play();
             break;
-        case "E":
+        case "e":
             var bass = new Audio('./sounds/basse.mp3');
             bass.play();
             break;
-        case "A":
+        case "a":
             var floor = new Audio('./sounds/floora.mp3');
             floor.play();
             break;
-        case "S":
+        case "s":
             var small = new Audio('./sounds/smalls.mp3');
             small.play();
             break;
-        case "D":
+        case "d":
             var ride = new Audio('./sounds/rided.mp3');
             ride.play();
             break;
-        case "Z":
+        case "z":
             var hat = new Audio('./sounds/hatz.mp3');
             hat.play();
             break;
-        case "X":
+        case "x":
             var hat2 = new Audio('./sounds/hatx.mp3');
             hat2.play();
             break;
-        case "C":
+        case "c":
             var tom = new Audio('./sounds/tom-4.mp3');
             tom.play();
             break;
-        default:
-            alert("Enable Capslock    /    hold  (Shift + q w e a s d z x c) ");
     }
 }
 
